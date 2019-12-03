9050f4ccb46b4ac2b3e3236a35107a87a0d3d6e5
Mikael <test.kurs@hotmail.net>
2019-12-03 15:57:15 -0800

    Add project files.

---
+++ CoreIntroNC19Egen/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreIntroNC19Egen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIntroNC19Egen.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index(int salary, string name)
+        {
+            //return $"Demo Index id:{id}";
+            //return HtmlEncoder.Default.Encode($"Demo index id:{id} name {name}");
+
+            //ViewData["id"] = id;
+            //ViewData ["name"] = name;
+
+            var model = new Person()
+            {
+                Name = name,
+                Salary = salary
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ CoreIntroNC19Egen/Data/CoreIntroNC19EgenContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreIntroNC19Egen.Models
+{
+    public class CoreIntroNC19EgenContext : DbContext
+    {
+        public CoreIntroNC19EgenContext (DbContextOptions<CoreIntroNC19EgenContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreIntroNC19Egen.Models.Person> Person { get; set; }
+    }
+}
